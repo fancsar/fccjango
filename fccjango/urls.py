c50b720088262b588bcde726fd7c764a4a424bf3
fancsar <m13821774377@163.com>
2020-04-27 23:04:03 +0800

    项目名更正

--- fccjango/urls.py
+++ fccjango/urls.py
@@ -1,4 +1,4 @@
-"""learnDjango URL Configuration
+"""fccjango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
